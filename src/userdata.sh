1f1faf77190e6c2c80fe7e44e173fa4a8b987ee4
badlydrawnface <bdface@proton.me>
2024-10-30 09:52:08 -0400

    fix user data

--- src/userdata.sh
+++ src/userdata.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-yum install -y git python3
+dnf in -y git python3
 git clone https://github.com/cs399f24/rocketchat-ec2
-cd rocketchat-ec2/src
+cd rocketchat-ec2
 python3 -m venv .venv
 source .venv/bin/activate
-pip install -r requirements.txt
-cp chatroom.service /etc/systemd/system
+pip install -r src/requirements.txt
+cp src/chatroom.service /etc/systemd/system
 systemctl enable chatroom
 systemctl start chatroom
