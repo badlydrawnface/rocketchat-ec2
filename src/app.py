10e146d6f27f3cf3d64b5549d36dad643b42140b
dg25moravian <99268114+dg25moravian@users.noreply.github.com>
2024-10-30 09:17:48 -0400

    Update app.py
    added CORS policy

--- src/app.py
+++ src/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, send_from_directory, render_template
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app, resources={r"/api/*": {"origins": "http://127.0.0.1:5500"}})##set this to website url
 
 # serve the application on the root path
 @app.route('/')
