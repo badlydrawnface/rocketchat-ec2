3447c0a01329584baa01c3d17bf30f19cfc5f914
Fernando Peralta Castro <73323113+fpera0248@users.noreply.github.com>
2024-11-08 09:24:49 -0500

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
-dnf in -y git python3 docker
+# Update the system and install necessary packages
+dnf install -y git python3 docker
+
+# Enable and start Docker service
 systemctl enable --now docker
-su ec2-user -c "git clone https://github.com/cs399f24/rocketchat-ec2.git"
-cd rocketchat-ec2
-docker build -t rocket .
-docker run -d -p 80:80 rocket
+
+# Add ec2-user to the Docker group
+usermod -aG docker ec2-user
+
+# Refresh the group permissions (this might not take effect until next login)
+newgrp docker
+
+# Clone the GitHub repository as ec2-user
+su - ec2-user -c "git clone https://github.com/cs399f24/chatroom_ec2_flask_docker_s3"
+
+# Change to the repository directory
+cd /home/ec2-user/chatroom_ec2_flask_docker_s3
+
+# Build the Docker image
+docker build -t flask-chat .
+
+# Run the Docker container
+docker run -d -p 80:80 flask-chat
